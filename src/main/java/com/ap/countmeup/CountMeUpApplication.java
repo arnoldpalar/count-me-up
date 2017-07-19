5f5a7ddb74038bd8c99c2e422f75e752816407fb
Arnold Palar <arnold.palar@gmail.com>
2017-07-19 22:54:51 +0800

    init

---
+++ src/main/java/com/ap/countmeup/CountMeUpApplication.java
@@ -0,0 +1,12 @@
+package com.ap.countmeup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountMeUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountMeUpApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ap/countmeup/CountMeUpApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ap.countmeup;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CountMeUpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
